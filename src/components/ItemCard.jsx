5eddb3b439588732ae0d33137817153463c6ca24
IstiakAR <istiakrhyme840@gmail.com>
2025-08-20 00:17:38 +0600

    added cart

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,5 +1,6 @@
 import '../styles/ItemCard.css';
 import { useNavigate } from 'react-router-dom';
+import { addToCart } from './CartCondition';
 
 function ItemCard({prop}) {
     const navigate = useNavigate();
@@ -7,13 +8,19 @@ function ItemCard({prop}) {
     const handleItemClick = () => {
         navigate(`/item/${prop.id}`);
     };
+    
+    const handleCartAdd = (e) => {
+        e.stopPropagation();
+        addToCart(prop.id, 1, 'plus');
+    }
+    
     return(
         <div className="item-card" onClick={handleItemClick}>
             <img src={prop.image} alt={prop.name} className="item-image" />
             <h3 className="item-name">{prop.name}</h3>
             <p className="item-price">₹{prop.price}</p>
             <p className='item-stock'>{prop.stock} left</p>
-            <button className="addCart" disabled={prop.stock === 0}>Add to Cart</button>
+            <button className="addCart" disabled={prop.stock === 0} onClick={handleCartAdd}>Add to Cart</button>
         </div>
     )
 }

bb8f845c8e845fa5fc5b981277b1db8bd5575947
IstiakAR <istiakrhyme840@gmail.com>
2025-10-24 19:27:46 +0600

    reset pass

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -7,12 +7,19 @@ import { useEffect } from 'react';
 
 function ForgotPassword() {
     const [email, setEmail] = useState('');
+    const [noError, setNoError] = useState(false);
+
     const handleEdit = (e) => {
         setEmail(e.target.value);
     }
     const handleSubmit = async () => {
+        setNoError(true);
+        const redirect = window.location.origin + '/reset-password';
         const { data, error } = await supabase.auth
-        .resetPasswordForEmail(email, {redirectTo: 'http://localhost:5173/reset-password'});
+        .resetPasswordForEmail(email, {redirectTo: redirect});
+        if(error){
+            setNoError(false);
+        }
     }
     return(
         <div className="login-container">
@@ -23,13 +30,16 @@ function ForgotPassword() {
                     </button>
                 </Link>
             </div>
+            {noError? (
+                <div>Password reset email sent successfully!</div>
+            ):
             <div className="form-container">
                 <h2>Enter email to reset your password</h2>
                 <div className="form-group">
                     <label htmlFor="email">Email:</label>
                     <input type="email" id="email" name="email" onChange={(e) => handleEdit(e)} required />
                 </div>
-                <button className="submit-btn" onClick={handleSubmit}>Submit</button>
+                <button className="submit-btn" onClick={handleSubmit} disabled={noError}>Submit</button>
                 <div className="toggle-form">
                     <p>Remember your password?{' '}
                         <Link to="/login">
@@ -38,6 +48,7 @@ function ForgotPassword() {
                     </p>
                 </div>
             </div>
+            }
         </div>
     )
 }
